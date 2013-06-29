9a44f11f879c9f0948497172c56d070744dc5fdd
elia222 <elia.lerda@gmail.com>
2013-06-29 11:38:05 +0200

    changes for slim

--- slim.mk
+++ slim.mk
@@ -1,14 +1,17 @@
 $(call inherit-product, device/sony/kumquat/full_kumquat.mk)
 
 # Inherit CM common GSM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/slim/config/gsm.mk)
 
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ST25i_1262-1120 BUILD_FINGERPRINT=SEMC/ST25i_1262-1120/ST25i:4.0.4/6.1.1.B.1.10/9bt3zw:user/release-keys PRIVATE_BUILD_DESC="ST25i-user 4.0.4 6.1.1.B.1.10 9bt3zw test-keys"
 
 TARGET_BOOTANIMATION_NAME := vertical-480x854
 
-PRODUCT_NAME := cm_kumquat
+PRODUCT_NAME := slim_kumquat
 PRODUCT_DEVICE := kumquat
+PRODUCT_BRAND := Sony
+PRODUCT_MODEL := Xperia U
+PRODUCT_MANUFACTURER := Sony
